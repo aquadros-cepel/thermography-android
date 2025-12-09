2c7a296ed88abd02b57f4c8d8b6a286003cb2fd0
André Quadros <aquadros@cepel.br>
2025-12-08 22:55:00 -0300

    feat: Registro do InstantAdapter e LocalDateAdapter no NetworkModule

--- app/src/main/java/com/tech/thermography/android/di/NetworkModule.kt
+++ app/src/main/java/com/tech/thermography/android/di/NetworkModule.kt
@@ -1,5 +1,9 @@
 package com.tech.thermography.android.di
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.tech.thermography.android.data.remote.adapter.InstantAdapter
+import com.tech.thermography.android.data.remote.adapter.LocalDateAdapter
 import com.tech.thermography.android.data.remote.auth.AuthApi
 import com.tech.thermography.android.data.remote.auth.AuthInterceptor
 import com.tech.thermography.android.data.remote.sync.SyncApi
@@ -10,10 +14,21 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.time.Instant
+import java.time.LocalDate
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson =
+        GsonBuilder()
+            .registerTypeAdapter(Instant::class.java, InstantAdapter())
+            .registerTypeAdapter(LocalDate::class.java, LocalDateAdapter())
+            .create()
 
     @Provides
     fun provideOkHttp(interceptor: AuthInterceptor): OkHttpClient =
@@ -22,11 +37,10 @@ object NetworkModule {
             .build()
 
     @Provides
-    fun provideRetrofit(client: OkHttpClient): Retrofit =        
+    fun provideRetrofit(client: OkHttpClient, gson: Gson): Retrofit =        
         Retrofit.Builder()
-            .baseUrl("http://192.168.15.172:8080/api/")
-//            .baseUrl("http://35.247.197.28:8080/api/")
-            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("http://34.95.132.119:8080/api/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .client(client)
             .build()
 
