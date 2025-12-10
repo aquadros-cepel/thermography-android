800394742f79aab0e5be66f6906e67d931e7e3c2
André Quadros <aquadros@cepel.br>
2025-12-10 18:24:55 -0300

    refactor: Refiz a ordem de inclusão do @Binds @IntoMap @IntKey(1) da classe mais acima, Company, UserInfo, Plants, Equipments, etc... para garantir que posso percorrer esse mapa em ordem @IntKey(1), que não vou violar integridade de FK gravando dados no Room

--- app/src/main/java/com/tech/thermography/android/di/RepositoryModule.kt
+++ app/src/main/java/com/tech/thermography/android/di/RepositoryModule.kt
@@ -1,14 +1,34 @@
 package com.tech.thermography.android.di
 
-import com.tech.thermography.android.data.local.repository.*
+import com.tech.thermography.android.data.local.repository.BusinessUnitRepository
+import com.tech.thermography.android.data.local.repository.CompanyRepository
+import com.tech.thermography.android.data.local.repository.EquipmentComponentRepository
+import com.tech.thermography.android.data.local.repository.EquipmentComponentTemperatureLimitsRepository
+import com.tech.thermography.android.data.local.repository.EquipmentGroupRepository
+import com.tech.thermography.android.data.local.repository.EquipmentRepository
+import com.tech.thermography.android.data.local.repository.EquipmentTypeTranslationRepository
+import com.tech.thermography.android.data.local.repository.InspectionRecordGroupEquipmentRepository
+import com.tech.thermography.android.data.local.repository.InspectionRecordGroupRepository
+import com.tech.thermography.android.data.local.repository.InspectionRecordRepository
+import com.tech.thermography.android.data.local.repository.InspectionRouteGroupEquipmentRepository
+import com.tech.thermography.android.data.local.repository.InspectionRouteGroupRepository
+import com.tech.thermography.android.data.local.repository.InspectionRouteRepository
+import com.tech.thermography.android.data.local.repository.PlantRepository
+import com.tech.thermography.android.data.local.repository.ROIRepository
+import com.tech.thermography.android.data.local.repository.RiskPeriodicityDeadlineRepository
+import com.tech.thermography.android.data.local.repository.RiskRecommendationTranslationRepository
+import com.tech.thermography.android.data.local.repository.SyncableRepository
+import com.tech.thermography.android.data.local.repository.ThermogramRepository
+import com.tech.thermography.android.data.local.repository.ThermographicInspectionRecordRepository
+import com.tech.thermography.android.data.local.repository.UserInfoRepository
 import com.tech.thermography.android.data.remote.auth.AuthRepository
 import com.tech.thermography.android.data.remote.auth.AuthRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import dagger.multibindings.IntoMap
 import dagger.multibindings.IntKey
+import dagger.multibindings.IntoMap
 import javax.inject.Singleton
 
 @Module
@@ -25,60 +45,60 @@ abstract class RepositoryModule {
     abstract fun bindCompanyRepository(impl: CompanyRepository): SyncableRepository
 
     @Binds @IntoMap @IntKey(2)
-    abstract fun bindBusinessUnitRepository(impl: BusinessUnitRepository): SyncableRepository
+    abstract fun bindUserInfoRepository(impl: UserInfoRepository): SyncableRepository
 
     @Binds @IntoMap @IntKey(3)
-    abstract fun bindPlantRepository(impl: PlantRepository): SyncableRepository
+    abstract fun bindBusinessUnitRepository(impl: BusinessUnitRepository): SyncableRepository
 
     @Binds @IntoMap @IntKey(4)
-    abstract fun bindEquipmentRepository(impl: EquipmentRepository): SyncableRepository
+    abstract fun bindPlantRepository(impl: PlantRepository): SyncableRepository
 
     @Binds @IntoMap @IntKey(5)
     abstract fun bindEquipmentGroupRepository(impl: EquipmentGroupRepository): SyncableRepository
 
     @Binds @IntoMap @IntKey(6)
-    abstract fun bindEquipmentTypeTranslationRepository(impl: EquipmentTypeTranslationRepository): SyncableRepository
+    abstract fun bindEquipmentRepository(impl: EquipmentRepository): SyncableRepository
 
     @Binds @IntoMap @IntKey(7)
-    abstract fun bindRiskPeriodicityDeadlineRepository(impl: RiskPeriodicityDeadlineRepository): SyncableRepository
-
-    @Binds @IntoMap @IntKey(8)
-    abstract fun bindRiskRecommendationTranslationRepository(impl: RiskRecommendationTranslationRepository): SyncableRepository
-
-    @Binds @IntoMap @IntKey(9)
-    abstract fun bindROIRepository(impl: ROIRepository): SyncableRepository
-
-    @Binds @IntoMap @IntKey(10)
-    abstract fun bindThermogramRepository(impl: ThermogramRepository): SyncableRepository
-
-    @Binds @IntoMap @IntKey(11)
-    abstract fun bindUserInfoRepository(impl: UserInfoRepository): SyncableRepository
-    
-    @Binds @IntoMap @IntKey(12)
     abstract fun bindInspectionRouteRepository(impl: InspectionRouteRepository): SyncableRepository
 
-    @Binds @IntoMap @IntKey(13)
-    abstract fun bindInspectionRecordRepository(impl: InspectionRecordRepository): SyncableRepository
-
-    @Binds @IntoMap @IntKey(14)
-    abstract fun bindEquipmentComponentRepository(impl: EquipmentComponentRepository): SyncableRepository
-
-    @Binds @IntoMap @IntKey(15)
+    @Binds @IntoMap @IntKey(8)
     abstract fun bindInspectionRouteGroupRepository(impl: InspectionRouteGroupRepository): SyncableRepository
 
-    @Binds @IntoMap @IntKey(16)
-    abstract fun bindInspectionRecordGroupRepository(impl: InspectionRecordGroupRepository): SyncableRepository
-
-    @Binds @IntoMap @IntKey(17)
+    @Binds @IntoMap @IntKey(9)
     abstract fun bindInspectionRouteGroupEquipmentRepository(impl: InspectionRouteGroupEquipmentRepository): SyncableRepository
 
-    @Binds @IntoMap @IntKey(18)
+    @Binds @IntoMap @IntKey(10)
+    abstract fun bindInspectionRecordRepository(impl: InspectionRecordRepository): SyncableRepository
+
+    @Binds @IntoMap @IntKey(11)
+    abstract fun bindInspectionRecordGroupRepository(impl: InspectionRecordGroupRepository): SyncableRepository
+
+    @Binds @IntoMap @IntKey(12)
     abstract fun bindInspectionRecordGroupEquipmentRepository(impl: InspectionRecordGroupEquipmentRepository): SyncableRepository
 
-    @Binds @IntoMap @IntKey(19)
+    @Binds @IntoMap @IntKey(13)
+    abstract fun bindEquipmentComponentRepository(impl: EquipmentComponentRepository): SyncableRepository
+
+    @Binds @IntoMap @IntKey(14)
+    abstract fun bindEquipmentComponentTemperatureLimitsRepository(impl: EquipmentComponentTemperatureLimitsRepository): SyncableRepository
+
+    @Binds @IntoMap @IntKey(15)
+    abstract fun bindThermogramRepository(impl: ThermogramRepository): SyncableRepository
+
+    @Binds @IntoMap @IntKey(16)
+    abstract fun bindROIRepository(impl: ROIRepository): SyncableRepository
+
+    @Binds @IntoMap @IntKey(17)
     abstract fun bindThermographicInspectionRecordRepository(impl: ThermographicInspectionRecordRepository): SyncableRepository
 
+    @Binds @IntoMap @IntKey(18)
+    abstract fun bindEquipmentTypeTranslationRepository(impl: EquipmentTypeTranslationRepository): SyncableRepository
+
+    @Binds @IntoMap @IntKey(19)
+    abstract fun bindRiskPeriodicityDeadlineRepository(impl: RiskPeriodicityDeadlineRepository): SyncableRepository
+
     @Binds @IntoMap @IntKey(20)
-    abstract fun bindEquipmentComponentTemperatureLimitsRepository(impl: EquipmentComponentTemperatureLimitsRepository): SyncableRepository
+    abstract fun bindRiskRecommendationTranslationRepository(impl: RiskRecommendationTranslationRepository): SyncableRepository
 
 }
