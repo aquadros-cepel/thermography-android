4bb0a3c5a3d576715d8d256e1122490516e355cd
André Quadros <aquadros@cepel.br>
2026-06-04 11:07:01 -0300

    fix: A animação da bolinha "Conectado à câmera FLIR..." estava redesenhando toda a UI a cad 1,5 s, e congelando as listas de componetes e da condição. (.alpha(dotAlpha)
    Substituído .graphicsLayer {alpha = dotAlpha}, que usa a GPU para renderizar separado esta parte

--- app/src/main/java/com/tech/thermography/android/ui/thermal_anomaly/ThermalAnomalyForm.kt
+++ app/src/main/java/com/tech/thermography/android/ui/thermal_anomaly/ThermalAnomalyForm.kt
@@ -42,6 +42,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -200,7 +201,9 @@ fun ThermalAnomalyForm(
                     tint = Color(0xFF00BCD4),
                     modifier = Modifier
                         .size(10.dp)
-                        .alpha(dotAlpha)
+                        .graphicsLayer {
+                            alpha = dotAlpha
+                        }
                 )
                 Text(
                     text = "Conectado à câmera $flirConnectedCameraName",
